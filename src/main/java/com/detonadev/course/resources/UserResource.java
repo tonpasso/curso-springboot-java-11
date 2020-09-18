07b07a973b527c83b7ed575202be73fe4b63a780
Wellington Passo <wellpasso@gmail.com>
2020-09-18 16:07:19 -0300

    Service layer, component registration

--- src/main/java/com/detonadev/course/resources/UserResource.java
+++ src/main/java/com/detonadev/course/resources/UserResource.java
@@ -1,21 +1,34 @@
 package com.detonadev.course.resources;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.detonadev.course.entities.User;
+import com.detonadev.course.services.UserService;
 
 @RestController
 @RequestMapping(value = "/user")
 public class UserResource {
 	
+	@Autowired
+	private UserService service;
+	
 	@GetMapping
-	public ResponseEntity<User> findAll(){
-		User u = new User(1L, "Well", "well@gmail.com", "988887777", "12345");
-		return ResponseEntity.ok().body(u);
-		
+	public ResponseEntity<List<User>> findAll(){
+		List<User> list = service.findAll();
+		return ResponseEntity.ok().body(list);		
+	}
+	
+	@GetMapping(value = "/{id}")
+	public ResponseEntity<User> findById(@PathVariable Long id){
+		User obj = service.findById(id);
+		return ResponseEntity.ok().body(obj);
 	}
 
 }
